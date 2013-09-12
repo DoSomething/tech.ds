942952048da8546db31f3a04fc8753b83ced78aa
mmwtsn <mwatson@dosomething.org>
2013-09-12 04:04:38 -0400

    Wraps #post-title script in a function

--- assets/javascripts/post-title.js
+++ assets/javascripts/post-title.js
@@ -1,7 +1,24 @@
-$postTitle = $('#post-title');
-var title = $postTitle.html();
-var words = title.split(' ');
-$postTitle.empty();
-for (var i = 0; i <= words.length + 1; i++) {
-  $postTitle.prepend('<span class="title-' + i + '">' + words.pop() + '</span><br/>');
-}
+(function($) {
+
+  var titleGradient = function() {
+    // Cache #post-title
+    $postTitle = $('#post-title');
+
+    // Get #post-title contents
+    var title = $postTitle.html();
+
+    // Split #post-title contents into an array of strings
+    var words = title.split(' ');
+
+    // Empty #post-title before adding new elements
+    $postTitle.empty();
+
+    // Wrap #post-title contents with <span> elements and prepend them to the #post-title <h1> element
+    for (var i = 0; i <= words.length + 1; i++) {
+      $postTitle.prepend('<span class="title-' + i + '">' + words.pop() + '</span><br/>');
+    }
+  }
+  titleGradient();
+
+})(jQuery);
+
